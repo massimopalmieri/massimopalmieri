64cbbbcb2e2ed83ce58c5503e4a6ac53688fe609
Massimo Palmieri <massimopalmieri@gmail.com>
2022-03-25 22:10:09 +0000

    update layout

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,9 +2,14 @@ import {ReactNode} from 'react'
 import {Link} from 'remix'
 import data from '~/data.json'
 
+const formatter = new Intl.DateTimeFormat('en-GB', {
+  month: 'short',
+  year: 'numeric',
+})
+
 function SectionTitle({children}: {children: ReactNode}) {
   return (
-    <h2 className="-ml-24 inline-block bg-[rgb(240,255,0)] p-4 px-10 py-6 text-4xl font-thin">
+    <h2 className="inline-block bg-[rgb(240,255,0)] p-4 px-10 py-6 text-4xl font-thin md:-ml-24">
       {children}
     </h2>
   )
@@ -12,59 +17,70 @@ function SectionTitle({children}: {children: ReactNode}) {
 
 export default function Index() {
   return (
-    <div
-      style={{fontFamily: 'system-ui, sans-serif', lineHeight: '1.4'}}
-      className="prose mx-auto"
-    >
-      <h1 className="mt-36 text-6xl font-thin">Massimo Palmieri</h1>
-
-      <SectionTitle>Professional summary</SectionTitle>
-      <p className="lead font-serif">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus,
-        amet animi aperiam aut autem dolores in minus nihil nisi nobis, officiis
-        quia quibusdam vero? Beatae illum iste mollitia natus quos.
-      </p>
-      <p className="lead font-serif">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus,
-        amet animi aperiam aut autem dolores in minus nihil nisi nobis, officiis
-        quia quibusdam vero? Beatae illum iste mollitia natus quos.
-      </p>
-
-      <SectionTitle>Skills</SectionTitle>
-      <div>
-        {data.skills.map((skill) => (
-          <span key={skill} className="m-2 inline-block text-sm font-semibold">
-            {skill}
-          </span>
-        ))}
+    <div className="relative mb-24">
+      <div className="sticky top-0 mb-16 mt-36 bg-white/50 py-8 backdrop-blur-xl">
+        <div className="prose mx-auto">
+          <h1 className="text-6xl font-thin">
+            Massimo <span className="font-normal">Palmieri</span>
+          </h1>
+        </div>
       </div>
 
-      <SectionTitle>Work history</SectionTitle>
-      {data.history.map((item, index) => {
-        return (
-          <div key={index} className="relative -ml-12 pl-12 pb-2">
-            <h3 className="mt-0 overflow-hidden pt-6">
-              {item.employer} - {item.title}
-            </h3>
-            <p className="my-4 text-sm font-semibold">{item.date}</p>
-            <ul className="pl-0 font-serif">
-              {item.achievements.map((item) => (
-                <li key={item}>{item}</li>
-              ))}
-            </ul>
-          </div>
-        )
-      })}
+      <div className="prose mx-auto p-4">
+        <SectionTitle>Professional summary</SectionTitle>
+        <p className="lead font-serif">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus,
+          amet animi aperiam aut autem dolores in minus nihil nisi nobis,
+          officiis quia quibusdam vero? Beatae illum iste mollitia natus quos.
+        </p>
+        <p className="lead font-serif">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus,
+          amet animi aperiam aut autem dolores in minus nihil nisi nobis,
+          officiis quia quibusdam vero? Beatae illum iste mollitia natus quos.
+        </p>
 
-      <SectionTitle>Education</SectionTitle>
-      {data.education.map((item) => (
-        <div key={item.title}>
-          <div>
-            {item.year} - <span className="font-semibold">{item.school}</span>
-          </div>
-          <p>{item.title}</p>
+        <SectionTitle>Skills</SectionTitle>
+        <div>
+          {data.skills.map((skill) => (
+            <span
+              key={skill}
+              className="m-2 inline-block text-sm font-semibold"
+            >
+              {skill}
+            </span>
+          ))}
         </div>
-      ))}
+
+        <SectionTitle>Work history</SectionTitle>
+        {data.history.map((item, index) => {
+          return (
+            <div key={index} className="pb-2">
+              <h3 className="mt-0 overflow-hidden pt-6">
+                {item.employer} - {item.title}
+              </h3>
+              <p className="my-4 text-sm font-semibold">
+                {formatter.format(new Date(item.start))} —{' '}
+                {item.end ? formatter.format(new Date(item.end)) : 'Present'}
+              </p>
+              <ul className="pl-0 font-serif">
+                {item.achievements.map((item) => (
+                  <li key={item}>{item}</li>
+                ))}
+              </ul>
+            </div>
+          )
+        })}
+
+        <SectionTitle>Education</SectionTitle>
+        {data.education.map((item) => (
+          <div key={item.title}>
+            <div>
+              {item.year} - <span className="font-semibold">{item.school}</span>
+            </div>
+            <p>{item.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
